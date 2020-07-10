f20ef3c8781e168e976c1cbb50fbb06681999926
Julian Tonti-Filippini <julian.tonti@gmail.com>
2020-07-10 17:26:53 +0800

    skeleton

---
+++ projects/days_alive/index.js
@@ -0,0 +1,12 @@
+import {render, html, svg} from 'uhtml.js';
+
+https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.27.0/moment.min.js
+
+function today() {
+}
+
+function days_alive(year, month, day)
+{
+}
+
+<input type='date' onchange=
