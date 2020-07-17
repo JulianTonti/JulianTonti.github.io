e2c91425d77c0f564d4649c39f07dffcbc5f2475
Julian Tonti-Filippini <julian.tonti@gmail.com>
2020-07-17 15:44:12 +0800

    Update lazy_page.js

--- js/lazy_page.js
+++ js/lazy_page.js
@@ -1,16 +1,16 @@
-let title = document.title || '';
-document.body.className = 'markdown-body';
-document.head.innerHTML = `
-  <title>${title}</title>
+document.head.innerHTML += `
   <meta charset='utf-8'>
   <meta http-equiv='X-UA-Compatible' content='IE=edge'>
   <meta name='viewport' content='width=device-width, initial-scale=1'>
+  <style>body { visibility : hidden; }</style>
   <link rel='stylesheet' type='text/css' media='screen' href='https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/4.0.0/github-markdown.min.css'>
 `;
 let s = document.createElement('script');
 document.head.appendChild(s);
 s.onload = () => {
+  document.body.className = 'markdown-body';
   document.body.innerHTML = marked(document.body.innerHTML);
+  document.body.style.visibility = 'visible';
   s.parentNode.removeChild(s);
 }
 s.src = "https://cdn.jsdelivr.net/npm/marked/marked.min.js";
