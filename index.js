9c10bba8ad6552bcab6cb4d6705fe8f8d401ce8f
Julian Tonti-Filippini <julian.tonti@gmail.com>
2024-05-28 15:11:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const commands = {
   alert    : args => alert(args),
   display  : args => document.body.style.display = args,
-  markdown : args => import('js/markdown.js'),
+  markdown : args => import('./js/markdown.js'),
   redirect : args => location.assign(args),
-  watch    : args => import('js/watch.js'),
+  watch    : args => import('./js/watch.js'),
 };
 for (const {name,value} of document.querySelector('script[src="https://julians.page/index.js"]')?.attributes ?? []) {
   const func = name.toLowerCase().split('cmd-')[1];
